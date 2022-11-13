8e6510b4471d34369c09f6dc900656c60af4df02
Theo <ekunnusitheophilus@gmail.com>
2022-11-13 18:53:09 +0100

    Data population ongoing

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,10 +1,37 @@
 import { Section, Aside } from '../../shared';
 
-function HomeContainer() {
+const Card = ({ item }) => {
+  const { name, address, rating, updated, revierws } = item;
+  return (
+    <div className="">
+      <div className="">
+        <h4>{name}</h4>
+
+        <small>{address}</small>
+      </div>
+
+      <div className="">
+        <div className="">
+          <p className=""></p>
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function HomeContainer({ items }) {
+  console.log(items);
   return (
     <Section className={`h-screen overflow-hidden w-[100%] bg-white`}>
       <Aside>
-        <div className="text-white">Home</div>
+        <div className="py-4">
+          <div>
+            {items?.map((item, index) => {
+              return <Card key={index} item={item} />;
+            })}
+          </div>
+        </div>
       </Aside>
     </Section>
   );
