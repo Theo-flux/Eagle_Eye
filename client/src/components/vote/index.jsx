13111199e0e24bfb02c3ca8a0d8cb342efbd8396
Theo <ekunnusitheophilus@gmail.com>
2022-11-13 16:25:43 +0100

    Vote section

--- client/src/components/vote/index.jsx
+++ client/src/components/vote/index.jsx
@@ -12,11 +12,44 @@ function VoteContainer() {
             />
           </div>
 
-          <div className="mt-6 border-gray border-b">
+          <div className="mt-8 border-gray border-b">
             <SubTitle
-              className={`w-[100%]  max-w-[500px]`}
+              className={`w-[100%]  max-w-[600px]`}
               content={`Proposals strictly to strengthen security of the network`}
             />
+          </div>
+
+          <div className="flex flex-col mt-3">
+            <div class="w-fit mb-2 flex items-center">
+              <input
+                id="bordered-checkbox-1"
+                type="checkbox"
+                value=""
+                name="bordered-checkbox"
+                className="w-6 h-6 text-laurel bg-white rounded"
+              />
+              <label
+                for="bordered-checkbox-1"
+                class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Big bounty
+              </label>
+            </div>
+            <div class="w-fit flex items-center">
+              <input
+                id="bordered-checkbox-2"
+                type="checkbox"
+                value=""
+                name="bordered-checkbox-2"
+                className="w-6 h-6 text-blue-600 bg-gray-100 rounded"
+              />
+              <label
+                for="bordered-checkbox-2"
+                class="py-2 ml-2 w-full text-sm font-medium text-gray-900"
+              >
+                File law suit
+              </label>
+            </div>
           </div>
         </div>
       </Aside>
